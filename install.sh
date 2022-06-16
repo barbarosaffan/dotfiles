7490d866dce4f8ea568740d70ba0729f7c999d60
Barbaros YILDIRIM <barbarosayildirim@gmail.com>
2022-06-16 11:22:36 +0300

    stuff, sick of commit messages

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-sudo eopkg up # update cak
-sudo eopkg it -c system.devel # bunlar elzem
+sudo dnf update # sys update
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm # rpm free
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm # rpm nonfree
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo # flatpak
+
+# codecs
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install lame\* --exclude=lame-devel
+sudo dnf group upgrade --with-optional Multimedia
+
 
 # install packages.list
-sudo eopkg it -y $(grep -v '^#.*' packages.list)
+sudo dnf install -y $(grep -v '^#.*' packages.list)
 
 # symlink dotfiles
 stow cfg
